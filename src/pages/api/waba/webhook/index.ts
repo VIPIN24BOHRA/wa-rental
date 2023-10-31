b78eef027f12115da2e1c06d484967b922730e36
VIPIN BOHRA <vipinbohra321@gmail.com>
2023-10-31 15:36:24 +0530

    feat: added webhook

--- src/pages/api/waba/webhook/index.ts
+++ src/pages/api/waba/webhook/index.ts
@@ -1,33 +1,21 @@
 /* eslint-disable import/extensions */
 // import '@/modules/firebase/firebase';
 
-import bodyParser from 'body-parser';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-import { runMiddleware } from '@/utils/runMiddleware';
-
-async function handleGetRequest(req: NextApiRequest, res: NextApiResponse) {
-  const mode = req.query['hub.mode'];
-  const token = req.query['hub.verify_token'];
-  const challenge = req.query['hub.challenge'];
-  if (
-    mode &&
-    token &&
-    mode === 'subscribe' &&
-    token === process.env.WEBHOOK_VERIFY_TOKEN
-  ) {
-    res.status(200).send(challenge);
-  } else {
-    res.status(403).send('Invalid verify_token');
-  }
+async function handleGetRequest(_req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).send('ok');
 }
 
 async function handlePostRequest(req: NextApiRequest, res: NextApiResponse) {
-  // get raw request body
-  await runMiddleware(req, res, bodyParser.raw({ type: 'application/json' }));
-
-  // save the messages
   try {
+    const { app, type, payload } = req.body;
+
+    if (app === 'flatdhudho') {
+      // reply to user
+      console.log('reply to user from here');
+      console.log(app, type, payload);
+    }
     res.status(200).send('success');
   } catch (e) {
     // eslint-disable-next-line no-console
