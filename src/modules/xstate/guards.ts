d5021b61fadcf6aa93a5bea0b2cb180158b8db2e
VIPIN BOHRA <vipinbohra321@gmail.com>
2023-11-03 21:31:28 +0530

    feat: added guards for xstate

---
+++ src/modules/xstate/guards.ts
@@ -0,0 +1,7 @@
+import type { MachineConfig } from './machine.types';
+
+export const gaurdsFactory = (_machineConfig: MachineConfig): any => {
+  return {
+    invalidLocationSelected: () => false,
+  };
+};
