69972b7c8de77584b125af51ce29658d516494b1
VIPIN BOHRA <vipinbohra321@gmail.com>
2023-11-23 12:02:17 +0530

    fix: quick reply error

--- src/utils/replyHelper/messageParser.ts
+++ src/utils/replyHelper/messageParser.ts
@@ -5,7 +5,7 @@ export const parseMessage = (messageObj: any) => {
     phonenumber: messageObj.sender.phone,
     name: messageObj.sender.name ?? '',
     message:
-      messageObj.type === 'text' && messageObj.type === 'quick_reply'
+      messageObj.type === 'text' || messageObj.type === 'quick_reply'
         ? messageObj.payload.text
         : messageObj.type === 'button_reply' && messageObj.payload.postbackText
         ? `${messageObj.payload.title}:${messageObj.payload.postbackText}`
